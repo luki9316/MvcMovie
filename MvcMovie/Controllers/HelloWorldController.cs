8ae8db0788c59377dfee6084d063fb59fd744fee
luki9316 <luki9316@gmail.com>
2015-04-24 20:59:00 +0200

    Simple View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -10,16 +10,20 @@ namespace MvcMovie.Controllers
     {
         //
         // GET: /HelloWorld/
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b> default </b> action ...";
+            return View();
         }
         //
         // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
             //return "This is my Welcome action method...";
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            //return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
 	}
 }
