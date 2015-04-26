cb531bff67fb1386acbe63953a51976c815647ce
luki9316 <luki9316@gmail.com>
2015-04-27 00:06:15 +0200

    Add movie search

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-
+using System.ComponentModel.DataAnnotations;
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
